db07a85a1e3f851ca1b3a1f806f620806366fee6
daniyamz <terkimbiterungwa@gmail.com>
2026-06-01 07:24:43 +0100

    My server

--- server.go
+++ server.go
@@ -5,16 +5,21 @@ import (
 	"net/http"
 )
 
+// create a handle
 func homeHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "server is working!")
-	http.ServeContent()
+
+	// w is for send data back and r is for reading incoming data
+	fmt.Fprint(w, "Welcome to my Go server!")
 }
 
 func main() {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", homeHandler)
-	
+	//create a router
+	mux := http.NewServeMux() 
 
+	// the router is associated with the a handle
+	mux.HandleFunc("/", homeHandler)
+
+	// using router the engine is start on port 8080
 	fmt.Println("Server is running on http://localhost:8080")
 	http.ListenAndServe(":8080", mux)
 }
