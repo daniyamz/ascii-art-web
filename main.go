de24f2d87e6f203fd2a5ec30340885274b8bc053
daniyamz <terkimbiterungwa@gmail.com>
2026-06-04 05:05:20 +0100

    Update

--- main.go
+++ main.go
@@ -1,5 +1,21 @@
 package main
 
-func main(){
-	
+import (
+	"ascii-art-web/source/server"
+	"log"
+	"net/http"
+)
+
+func main() {
+	//created a new servemux
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", server.Hello)
+	serverAdress := ":8080"
+
+	err := http.ListenAndServe(serverAdress, mux)
+	//check when server fails to start
+	if err != nil {
+		log.Fatalf("Server failds to start: %v", err)
+	}
 }
--- source/server/server.go
+++ source/server/server.go
@@ -2,25 +2,9 @@ package server
 
 import (
 	"net/http"
-	"text/template"
 )
 
-type ErrorPage struct {
-	errcode string
-	errmsg  string
-}
-
-var Err = *ErrorPage
-
-func errHandler(w http.ResponseWriter, r *http.Request) {
-	errTmp := template.Must(template.ParseFiles("template/error.html"))
-	errTmp.Execute(w, Err)
-}
-func MainHandler(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
-		err := ErrorPage{errcode: "404", errmsg: "PAGE NOT FOUND"}
-		w.WriteHeader(http.StatusNotFound)
-		errHandler(w, r, &Err)
-		return
-	}
+// function to hand index.html in the server.
+func Hello(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "templates/index.html")
 }
