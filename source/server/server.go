b2230103b75a265839fc77ee8066cacec29af0fe
daniyamz <terkimbiterungwa@gmail.com>
2026-06-05 10:23:06 +0100

    Servers

--- source/server/server.go
+++ source/server/server.go
@@ -1,8 +1,36 @@
 package server
 
 import (
+	"html/template"
 	"net/http"
 )
+
+type ErrorPageMsg struct {
+	errorCode string
+	errorMsg  string
+}
+
+func errHandler(w http.ResponseWriter, r *http.Request, err *ErrorPageMsg) {
+	errTmp := template.Must(template.ParseFiles("template/index.html"))
+	errTmp.Execute(w, err)
+}
+
+// function to validate the main
+func MainHandler(w http.ResponseWriter, r *http.Request) {
+	// check for the path request
+	if r.URL.Path != "/" {
+		err := ErrorPageMsg{errorCode: "404", errorMsg: "PAGE NOT FOUND"}
+		w.WriteHeader(http.StatusFound)
+		errHandler(w, r, &err)
+		return
+	}
+	if r.Method != "Get" {
+		err := ErrorPageMsg{errorCode: "405", errorMsg: "METHOD NOT ALLOWED"}
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		errHandler(w, r, &err)
+		return
+	}
+}
 
 // function to hand index.html in the server.
 func Hello(w http.ResponseWriter, r *http.Request) {
