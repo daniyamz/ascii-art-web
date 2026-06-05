42af007a409b14b496ade90f5eb1d62e7298c6de
daniyamz <terkimbiterungwa@gmail.com>
2026-06-05 15:18:08 +0100

    Server

--- source/server/server.go
+++ source/server/server.go
@@ -10,6 +10,12 @@ type ErrorPageMsg struct {
 	errorMsg  string
 }
 
+type resPageData struct {
+	input  string
+	banner string
+	res    string
+}
+
 func errHandler(w http.ResponseWriter, r *http.Request, err *ErrorPageMsg) {
 	errTmp := template.Must(template.ParseFiles("template/index.html"))
 	errTmp.Execute(w, err)
@@ -34,11 +40,23 @@ func MainHandler(w http.ResponseWriter, r *http.Request) {
 	main, err := template.ParseFiles("template/index.html")
 	if err != nil {
 		err := ErrorPageMsg{errorCode: "500", errorMsg: "INTERNAL SERVER ERROR"}
+		w.WriteHeader(http.StatusMethodNotAllowed)
 		errHandler(w, r, &err)
 		return
 	}
 	mainTmp := template.Must(main, nil)
 	mainTmp.Execute(w, nil)
+}
+
+// validating respagagedata
+func ResHandler(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		err := ErrorPageMsg{errorCode: "500", errorMsg: "INTERNAL SERVER ERROR"}
+		w.WriteHeader(http.StatusInternalServerError)
+		errHandler(w, r, &err)
+		return
+	}
 }
 
 // function to hand index.html in the server.
